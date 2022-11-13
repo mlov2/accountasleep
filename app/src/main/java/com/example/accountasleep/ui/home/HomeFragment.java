fcefef089eb5771c51abde23e134f4efe9164249
yuchen9902 <yuchenz8@illinois.edu>
2022-11-13 13:46:20 -0600

    "Repeat button now can change the visibility of other layouts."

--- app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 
 import com.example.accountasleep.databinding.FragmentHomeBinding;
 
@@ -24,8 +28,23 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        LinearLayout alarm_header = binding.alarmHeaderLayout;
+        LinearLayout alarm_setting = binding.alarmSettingLayout;
+        LinearLayout alarm_repeat = binding.repeatSetterLayout;
+        LinearLayout alarm_snooze = binding.snoozeSetterLayout;
+        Button repeat_button = binding.repeatButton;
+
+        repeat_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                alarm_header.setVisibility(View.INVISIBLE);
+                alarm_setting.setVisibility(View.INVISIBLE);
+                alarm_repeat.setVisibility(View.VISIBLE);
+            }
+        });
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
