c5a239c341687310a66a08998e90fe5582102f7d
Megan Lov <mlov2@illinois.edu>
2022-11-13 21:27:42 -0600

    Create a basic (working) list view

--- app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
@@ -4,29 +4,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TimePicker;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-
+import com.example.accountasleep.R;
 import com.example.accountasleep.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    // Array of strings...
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
+            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+//        HomeViewModel homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // references:
+        // - to get listview to work: https://www.youtube.com/watch?v=7MRnL_slGrI
+        // - to create a custom adapter: TODO (refer to one of the links below)
+        ListView lv = (ListView) root.findViewById(R.id.alarm_list_view);
+        ArrayAdapter adapter=new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, mobileArray);
+//        CustomAdapter adapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+        lv.setAdapter(adapter);
+
+
+
+        // reference: https://www.tutorialspoint.com/android/android_list_view.htm
+        // reference: https://www.tutorialspoint.com/how-to-use-findviewbyid-in-fragment
+//        ViewGroup alarm_lst = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
+////        LinearLayout alarm_list = binding.alarmListLayout;
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.alarm_list, mobileArray);
+//        ListView listView = (ListView) alarm_lst.findViewById(R.id.alarm_list_list);
+//        listView.setAdapter(adapter);
+
+//        MyListAdapter adapter=new MyListAdapter(this, mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+//        list=(ListView)findViewById(R.id.list);
+//        list.setAdapter(adapter);
+
+        // Inflate the layout for this fragment - ref: https://stackoverflow.com/questions/69164648/adding-a-listview-in-a-fragment
+//        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
+//        ListView listView = contentView.findViewById(R.id.alarm_list_list);
+//        CustomAdapter listAdapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+//        listView.setAdapter(listAdapter);
 
         LinearLayout alarm_header = binding.alarmHeaderLayout;
         LinearLayout alarm_setting = binding.alarmSettingLayout;
