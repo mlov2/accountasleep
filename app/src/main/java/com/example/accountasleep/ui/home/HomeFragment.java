a7de540f0f4147d72d050cdcf7fc0e33b8d28deb
Megan Lov <mlov2@illinois.edu>
2022-11-17 21:03:15 -0600

    Add in functionality for editing an alarm; fix snooze setting functionality; fix alarm time display on alarm list page

--- app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/accountasleep/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -51,6 +53,22 @@ public class HomeFragment extends Fragment {
     private boolean save_snooze = false;
     private int save_snooze_duration = -1;
     private int save_snooze_limit = -1;
+    private int edit_position;
+
+    private void populateHardcodedAlarms(String alarm_label, String alarm_time, String alarm_time_of_day,
+                                         Boolean[] alarm_repeat, boolean send_a_msg, boolean snooze,
+                                         int snooze_duration, int snooze_limit) {
+        ArrayList<Object> alarm_raw = new ArrayList<>();
+        alarm_raw.add(alarm_label);
+        alarm_raw.add(alarm_time);
+        alarm_raw.add(alarm_time_of_day);
+        alarm_raw.add(alarm_repeat);
+        alarm_raw.add(send_a_msg);
+        alarm_raw.add(snooze);
+        alarm_raw.add(snooze_duration);
+        alarm_raw.add(snooze_limit);
+        alarms_raw.add(alarm_raw);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -59,45 +77,6 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        // references:
-        // - to get listview to work: https://www.youtube.com/watch?v=7MRnL_slGrI
-        // - to create a custom adapter: https://www.geeksforgeeks.org/custom-arrayadapter-with-listview-in-android/
-        lv = (ListView) root.findViewById(R.id.alarm_list_view);
-
-        alarmlist = new ArrayList<>();
-        alarmlist.add(new Alarm("8:00", "AM", "MW", "CS 465 lecture", true, 10, 3, true));
-        alarmlist.add(new Alarm("9:00", "AM", "TTh", "CS 421 HW", false, 8, 5, false));
-        alarmlist.add(new Alarm("12:00", "AM", "SMTWThFS", "Go to bed", false, -1, -1, true));
-
-//        ArrayAdapter adapter=new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, mobileArray);
-//        CustomAdapter adapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
-        adapter = new AlarmAdapter(this.getActivity(), alarmlist);
-        lv.setAdapter(adapter);
-
-        // references for adapter:
-        // - https://www.geeksforgeeks.org/custom-arrayadapter-with-listview-in-android/
-        // - https://medium.com/mindorks/custom-array-adapters-made-easy-b6c4930560dd
-
-
-
-        // reference: https://www.tutorialspoint.com/android/android_list_view.htm
-        // reference: https://www.tutorialspoint.com/how-to-use-findviewbyid-in-fragment
-//        ViewGroup alarm_lst = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
-////        LinearLayout alarm_list = binding.alarmListLayout;
-//        ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.alarm_list, mobileArray);
-//        ListView listView = (ListView) alarm_lst.findViewById(R.id.alarm_list_list);
-//        listView.setAdapter(adapter);
-
-//        MyListAdapter adapter=new MyListAdapter(this, mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
-//        list=(ListView)findViewById(R.id.list);
-//        list.setAdapter(adapter);
-
-        // Inflate the layout for this fragment - ref: https://stackoverflow.com/questions/69164648/adding-a-listview-in-a-fragment
-//        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
-//        ListView listView = contentView.findViewById(R.id.alarm_list_list);
-//        CustomAdapter listAdapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
-//        listView.setAdapter(listAdapter);
 
         // alarm list page variables
         LinearLayout alarm_page_header = binding.alarmPageHeaderLayout;
@@ -128,10 +107,29 @@ public class HomeFragment extends Fragment {
         NumberPicker snooze_limit_number_picker = binding.snoozeLimitNumberPicker;
         Button snooze_ok_button = binding.okButtonSnooze;
 
-        //Edit alarm
-        edit_alarm_button.setOnClickListener(new View.OnClickListener() {
+        // references:
+        // - to get listview to work: https://www.youtube.com/watch?v=7MRnL_slGrI
+        // - to create a custom adapter: https://www.geeksforgeeks.org/custom-arrayadapter-with-listview-in-android/
+        lv = (ListView) root.findViewById(R.id.alarm_list_view);
+
+        alarmlist = new ArrayList<>();
+        populateHardcodedAlarms("CS 465 lecture", "8:00", "AM", new Boolean[]{false, true, false, true, false, false, false}, true, true, 10, 3);
+        populateHardcodedAlarms("CS 465 studio", "8:00", "AM", new Boolean[]{false, false, false, false, false, true, false}, true, true, 8, 5);
+        populateHardcodedAlarms("Go to bed", "12:00", "AM", new Boolean[]{true, true, true, true, true, true, true}, false, false, -1, -1);
+        alarmlist.add(new Alarm("8:00", "AM", "MW", "CS 465 lecture", true, 10, 3, true));
+        alarmlist.add(new Alarm("8:00", "AM", "F", "CS 465 studio", true, 8, 5, false));
+        alarmlist.add(new Alarm("12:00", "AM", "SMTWThFS", "Go to bed", false, -1, -1, true));
+
+//        ArrayAdapter adapter=new ArrayAdapter(this.getActivity(), android.R.layout.simple_list_item_1, mobileArray);
+//        CustomAdapter adapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+        adapter = new AlarmAdapter(this.getActivity(), alarmlist);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                edit_position = position;
+
+                // change the page from the alarm list to alarm setting
                 // hide alarm list page
                 alarm_page_header.setVisibility(View.GONE);
                 alarm_list.setVisibility(View.GONE);
@@ -143,10 +141,137 @@ public class HomeFragment extends Fragment {
                 alarm_setting_header_value = "Edit Alarm";
                 alarm_setting_header_label.setText(alarm_setting_header_value);
 
-                // TODO: set fields in the alarm setting page to the
-                //  current settings of the alarm we are editing
+                // get all the settings for the alarm that was clicked
+                ArrayList<Object> curr_alarm = alarms_raw.get(position);
+                String alarm_label_setting = (String) curr_alarm.get(0);
+                String alarm_time_setting = (String) curr_alarm.get(1);
+                String alarm_time_of_day_setting = (String) curr_alarm.get(2);
+                Boolean[] alarm_repeat_setting = (Boolean[]) curr_alarm.get(3);
+                boolean send_msg_setting = (boolean) curr_alarm.get(4);
+                boolean snooze_setting = (boolean) curr_alarm.get(5);
+                int snooze_duration_setting = (int) curr_alarm.get(6);
+                int snooze_limit_setting = (int) curr_alarm.get(7);
+
+                // display the existing setting in the alarm setting page (backend and frontend)
+                alarm_label.setText(alarm_label_setting);
+
+                // set time setting
+                int hour = Integer.valueOf(alarm_time_setting.split(":")[0]);
+                int minute = Integer.valueOf(alarm_time_setting.split(":")[1]);
+                if (alarm_time_of_day_setting == "PM") {
+                    hour = hour + 12;
+                } else {
+                    if (hour == 12) {
+                        hour = 0;
+                    }
+                }
+                alarm_time.setCurrentHour(hour);
+                alarm_time.setCurrentMinute(minute);
+
+                // set repeat setting
+                binding.Sunday.setChecked(alarm_repeat_setting[0]);
+                binding.Monday.setChecked(alarm_repeat_setting[1]);
+                binding.Tuesday.setChecked(alarm_repeat_setting[2]);
+                binding.Wednesday.setChecked(alarm_repeat_setting[3]);
+                binding.Thursday.setChecked(alarm_repeat_setting[4]);
+                binding.Friday.setChecked(alarm_repeat_setting[5]);
+                binding.Saturday.setChecked(alarm_repeat_setting[6]);
+
+                String repeat_output = "";
+                if(binding.Monday.isChecked()){
+                    repeat_output += "MON ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Tuesday.isChecked()){
+                    repeat_output += "TUE ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Wednesday.isChecked()){
+                    repeat_output += "WED ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Thursday.isChecked()){
+                    repeat_output += "THU ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Friday.isChecked()){
+                    repeat_output += "FRI ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Saturday.isChecked()){
+                    repeat_output += "SAT ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+                if(binding.Sunday.isChecked()){
+                    repeat_output += "SUN ";
+                    repeat_button.setPadding(10,0,0,0);
+                }
+
+                if(repeat_output.length()==0){
+                    repeat_button.setText("NEVER");
+                }else{
+                    repeat_button.setText(repeat_output);
+                }
+
+                // set custom settings
+                send_msg_switch.setChecked(send_msg_setting);
+                snooze_switch.setChecked(snooze_setting);
+                snooze_duration_input = snooze_duration_setting;
+                snooze_limit_input = snooze_limit_setting;
+                snooze_duration_number_picker.setValue(snooze_duration_input);
+                snooze_limit_number_picker.setValue(snooze_limit_input);
+
+                String snooze_duration_output = snooze_duration_input + " mins";
+                duration_button.setText(snooze_duration_output);
+                String snooze_limit_output = snooze_limit_input + " times";
+                limit_button.setText(snooze_limit_output);
             }
         });
+
+        // references for adapter:
+        // - https://www.geeksforgeeks.org/custom-arrayadapter-with-listview-in-android/
+        // - https://medium.com/mindorks/custom-array-adapters-made-easy-b6c4930560dd
+
+
+
+        // reference: https://www.tutorialspoint.com/android/android_list_view.htm
+        // reference: https://www.tutorialspoint.com/how-to-use-findviewbyid-in-fragment
+//        ViewGroup alarm_lst = (ViewGroup) inflater.inflate(R.layout.fragment_home, null);
+////        LinearLayout alarm_list = binding.alarmListLayout;
+//        ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), R.layout.alarm_list, mobileArray);
+//        ListView listView = (ListView) alarm_lst.findViewById(R.id.alarm_list_list);
+//        listView.setAdapter(adapter);
+
+//        MyListAdapter adapter=new MyListAdapter(this, mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+//        list=(ListView)findViewById(R.id.list);
+//        list.setAdapter(adapter);
+
+        // Inflate the layout for this fragment - ref: https://stackoverflow.com/questions/69164648/adding-a-listview-in-a-fragment
+//        View contentView = inflater.inflate(R.layout.fragment_home, container, false);
+//        ListView listView = contentView.findViewById(R.id.alarm_list_list);
+//        CustomAdapter listAdapter = new CustomAdapter(mobileArray, mobileArray, mobileArray, mobileArray, mobileArray);
+//        listView.setAdapter(listAdapter);
+
+
+//        //Edit alarm
+//        edit_alarm_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // hide alarm list page
+//                alarm_page_header.setVisibility(View.GONE);
+//                alarm_list.setVisibility(View.GONE);
+//
+//                // show alarm setting page
+//                alarm_setting_page.setVisibility(View.VISIBLE);
+//
+//                // ensure alarm setting page header is set correctly
+//                alarm_setting_header_value = "Edit Alarm";
+//                alarm_setting_header_label.setText(alarm_setting_header_value);
+//
+//                // TODO: set fields in the alarm setting page to the
+//                //  current settings of the alarm we are editing
+//            }
+//        });
 
         //Add alarm
         add_alarm_button.setOnClickListener(new View.OnClickListener() {
@@ -193,7 +318,10 @@ public class HomeFragment extends Fragment {
                 save_alarm_label = alarm_label.getText().toString();
                 // alarm time
                 int hour = alarm_time.getCurrentHour();
-                if (hour > 12) {
+                if (hour == 0) {
+                    hour = 12;
+                    save_alarm_time_of_day = "AM";
+                } else if (hour > 12) {
                     hour = hour - 12;
                     save_alarm_time_of_day = "PM";
                 } else {
@@ -219,10 +347,8 @@ public class HomeFragment extends Fragment {
                 // custom
                 save_send_a_message = send_msg_switch.isChecked();
                 save_snooze = snooze_switch.isChecked();
-                if (save_snooze) {
-                    save_snooze_duration = snooze_duration_input;
-                    save_snooze_limit = snooze_limit_input;
-                }
+                save_snooze_duration = snooze_duration_input;
+                save_snooze_limit = snooze_limit_input;
 
                 // save alarm settings
                 ArrayList<Object> alarm_raw = new ArrayList<>();
@@ -234,7 +360,13 @@ public class HomeFragment extends Fragment {
                 alarm_raw.add(save_snooze);
                 alarm_raw.add(save_snooze_duration);
                 alarm_raw.add(save_snooze_limit);
-                alarms_raw.add(alarm_raw);
+
+                if (alarm_setting_header_value == "Add Alarm") {
+                    // add new alarm
+                    alarms_raw.add(alarm_raw);
+                } else {
+                    alarms_raw.set(edit_position, alarm_raw);
+                }
 
                 // update alarm list page
                 ArrayList<Object> current_alarm = alarm_raw;
@@ -263,13 +395,25 @@ public class HomeFragment extends Fragment {
                 int snooze_duration = (int) current_alarm.get(6);
                 int snooze_limit = (int) current_alarm.get(7);
 
-                alarmlist.add(new Alarm(alarm_time, alarm_time_of_day, alarm_repeat_str, alarm_label, send_msg, snooze_duration, snooze_limit, true));
+                if (alarm_setting_header_value == "Add Alarm") {
+                    // add new alarm
+                    alarmlist.add(new Alarm(alarm_time, alarm_time_of_day, alarm_repeat_str, alarm_label, send_msg, snooze_duration, snooze_limit, true));
+                } else {
+                    // replace old alarm with edited alarm settings
+                    alarmlist.set(edit_position, new Alarm(alarm_time, alarm_time_of_day, alarm_repeat_str, alarm_label, send_msg, snooze_duration, snooze_limit, true));
+                }
 
                 // reference to refresh list view: https://stackoverflow.com/questions/37460133/how-to-refresh-listview-in-a-custom-adapter
                 lv.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
                 lv.invalidateViews();
                 lv.refreshDrawableState();
+
+                if (alarm_setting_header_value == "Add Alarm") {
+                    Toast.makeText(getContext(), "Alarm Created", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), "Alarm Edited", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -288,8 +432,24 @@ public class HomeFragment extends Fragment {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     snooze_card.setVisibility(buttonView.VISIBLE);
+                    snooze_duration_input = 5;
+                    snooze_limit_input = 1;
+
+                    // display in alarm setting page
+                    String snooze_duration_output = snooze_duration_input + " mins";
+                    duration_button.setText(snooze_duration_output);
+                    String snooze_limit_output = snooze_limit_input + " times";
+                    limit_button.setText(snooze_limit_output);
                 } else {
                     snooze_card.setVisibility(buttonView.INVISIBLE);
+                    snooze_duration_input = -1;
+                    snooze_limit_input = -1;
+
+                    // display in alarm setting page
+                    String snooze_duration_output = snooze_duration_input + " mins";
+                    duration_button.setText(snooze_duration_output);
+                    String snooze_limit_output = snooze_limit_input + " times";
+                    limit_button.setText(snooze_limit_output);
                 }
             }
         });
@@ -396,6 +556,10 @@ public class HomeFragment extends Fragment {
 
                 // show alarm snooze setting page
                 alarm_snooze.setVisibility(View.VISIBLE);
+
+                // set the values for the pickers
+                snooze_duration_number_picker.setValue(snooze_duration_input);
+                snooze_limit_number_picker.setValue(snooze_limit_input);
             }
         });
 
